ed58a287187027688a7fb8db9fe38499b43a1dff
ganjos <gmmanjos@gmail.com>
2025-01-10 13:35:31 -0300

    Projeto PHP - Desenvolvendo templates padrão footer e header

---
+++ projeto-controle-funcionarios/templates/footer.php
@@ -0,0 +1,5 @@
+<footer>
+        <p>2025 Projeto PHP - Controle de Funcionários</p>
+    </footer>
+</body>
+</html>
---
+++ projeto-controle-funcionarios/templates/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Controle de Funcionários</title>
+    <link rel="stylesheet" href="../public/styles.css">
+</head>
+<body>
+    <header>
+        <h1>Controle de Funcionários</h1>
+    </header>
